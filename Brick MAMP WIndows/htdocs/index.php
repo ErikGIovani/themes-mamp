63a820a0a5dccd2654e7d668690bd72e751afc3d
ErikGIovani <46170949+ErikGIovani@users.noreply.github.com>
2019-12-23 19:43:44 -0600

    Add files via upload

---
+++ Brick MAMP WIndows/htdocs/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en" >
+
+<head>
+  <meta charset="UTF-8">
+  <link rel="shortcut icon" href="Files/css/favicon.ico">
+  <title>My Web Containers</title>
+  
+  
+  
+      <link rel="stylesheet" href="Files/css/style.css">
+
+  
+</head>
+
+<body>
+
+  <aside>
+  <img src="Files/css/627.png" alt="">
+  <h1>My Web Containers</h1>
+</aside>
+<input type="checkbox" id="menu-toggle"/>
+  <label id="trigger" for="menu-toggle"></label>
+  <label id="burger" for="menu-toggle"></label>
+  <ul id="menu">
+    <li><a href="#">Web Container #1</a></li>
+    <li><a href="#">Web Container #2</a></li>
+    <li><a href="#">Web Container #3</a></li>
+    <li><a href="#">Web Container #4</a></li>
+  </ul>
+  
+</body>
+
+</html>
